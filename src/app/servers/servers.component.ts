d34ec77831564ad2210908f067552cf2af761b19
oswaldo garcia <garcia.oswaldo1000@gmail.com>
2021-12-21 13:01:05 -0600

    learned about template Url and template inline with tick marks to allow for small htmo page.

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,7 +2,12 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  //you can have templateUrl or template but can not be missing.
+  // templateUrl: './servers.component.html',
+  // if using internal webpage use templateUrl otherwise use back ticks for an inline template
+  //template: `<app-server></app-server><app-server></app-server>`,
+
+  template: '<app-server></app-server><app-server></app-server>',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
