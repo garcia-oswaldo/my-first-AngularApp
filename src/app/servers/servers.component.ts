10ab3b8e407f26e4caa65ad034cadee032a7a06f
oswaldo garcia <garcia.oswaldo1000@gmail.com>
2021-12-21 14:11:36 -0600

    changed selector to an attribute and to a class for use in html by way of the component.ts

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,12 +2,15 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+  // selector: '.app-servers',
   //you can have templateUrl or template but can not be missing.
   // templateUrl: './servers.component.html',
   // if using internal webpage use templateUrl otherwise use back ticks for an inline template
   //template: `<app-server></app-server><app-server></app-server>`,
 
-  template: '<app-server></app-server><app-server></app-server>',
+  template: '' +
+    '<app-server></app-server>' +
+    '<app-server></app-server>',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
