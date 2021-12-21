195c2179396ac6bd859f293d7e4918aafa081241
oswaldo garcia <garcia.oswaldo1000@gmail.com>
2021-12-21 13:24:00 -0600

    learned about styling html via styleUrls external and by directly styling in component.ts'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+  // styles allows you to code css in the component.ts files
+  styles: [`
+  h3{
+    color: dodgerblue;
+  }
+  `]
 })
 export class AppComponent {
   // title = 'my-first-app';
